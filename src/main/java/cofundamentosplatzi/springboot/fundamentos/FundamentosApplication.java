c02417e4e3a15e193ef27c4c13ecd10f7f200fa0
ricardocema <ricardo_cema@hotmail.com>
2022-07-27 16:01:10 -0500

    first commit

---
+++ src/main/java/cofundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -0,0 +1,13 @@
+package cofundamentosplatzi.springboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
